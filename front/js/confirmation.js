97d1e0d328e96390be0a9060b48f6b431b2bd303
Garance Defrel <garance972@hotmail.com>
2022-10-10 16:15:05 +0200

    Final Code

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -4,11 +4,6 @@ let orderId = new URL(confirmation);
 let orderIdConfirmation = orderId.searchParams.get("orderId");
 console.log(orderId);
 
-//Une fonction qui permet d'afficher le numéro de commande du client
-function showConfiramtionId() {
-  //Déclaration de la variable qui affichera le numéro de la commande
-  let displayOrderId = document.querySelector("#orderId");
-  displayOrderId.innerText = orderIdConfirmation;
-}
-//Appel de la fonction permettant d'afficher le numéro de commande
-showConfiramtionId();
+let displayOrderId = document.querySelector("#orderId");
+displayOrderId.innerText = orderIdConfirmation;
+localStorage.clear();
